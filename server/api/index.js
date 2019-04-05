f5274b4ac5d6c219677a86038f82f79cf08bff81
Rick Terry <fdterr@gmail.com>
2019-04-05 17:11:38 -0400

    able to get list of all highlights with player

--- server/api/index.js
+++ server/api/index.js
@@ -24,38 +24,52 @@ router.use('/teams', async (req, res, next) => {
   }
 });
 
-router.use('/players', async (req, res, next) => {
+router.get('/createList', async (req, res, next) => {
+  const teams = await getTeams();
+  // console.log('teams is', teams);
+  const players = [];
+  for (let i = 0; i < teams.length; i++) {
+    const id = teams[i];
+    const {data} = await axios.get(
+      `http://statsapi.mlb.com/api/v1/teams/${id}/roster/fullSeason?season=2019`
+    );
+    const roster = data.roster;
+
+    await roster.forEach(async player => {
+      const newPlayer = {
+        playerId: player.person.id,
+        fullName: player.person.fullName,
+        team: teams[i],
+        season: 2019
+      };
+
+      await Player.findOrCreate({
+        where: {playerId: player.person.id},
+        defaults: {...newPlayer}
+      });
+    });
+  }
+});
+
+router.get('/players', async (req, res, next) => {
   try {
-    // const teams = await getTeams();
-    // console.log('teams is', teams);
-    // const players = [];
-    // for (let i = 0; i < teams.length; i++) {
-    //   const id = teams[i];
-    //   const {data} = await axios.get(
-    //     `http://statsapi.mlb.com/api/v1/teams/${id}/roster/fullSeason?season=2019`
-    //   );
-    //   const roster = data.roster;
-
-    //   await roster.forEach(async player => {
-    //     const newPlayer = {
-    //       playerId: player.person.id,
-    //       fullName: player.person.fullName,
-    //       team: teams[i],
-    //       season: 2019
-    //     };
-
-    //     await Player.findOrCreate({
-    //       where: {playerId: player.person.id},
-    //       defaults: {...newPlayer}
-    //     });
-    //   });
-    // }
     const players = await Player.findAll();
     // console.log('players are', players);
     res.json(players);
   } catch (err) {
     next(err);
   }
+});
+
+router.get('/player/:id/games', async (req, res, next) => {
+  const games = await getPlayerGames(req.params.id, 'pitching', '2019');
+  res.json(games);
+});
+
+router.get('/player/:id/content', async (req, res, next) => {
+  const games = await getPlayerGames(req.params.id, 'pitching', '2019');
+  const content = await getPlayerContent('Jacob deGrom', 567365);
+  res.json(content);
 });
 
 router.use((req, res, next) => {
@@ -78,4 +92,39 @@ const getTeams = async () => {
     }
   });
   return teams;
+};
+
+const getPlayerGames = async (player, group, season) => {
+  const {data} = await axios.get(
+    `http://statsapi.mlb.com/api/v1/people/${player}/stats?stats=gameLog&season=${season}`
+  );
+  // console.log('data is', data);
+  const splits = data.stats[0].splits;
+  const games = [];
+  splits.forEach(game => {
+    const gamePk = game.game.gamePk;
+    games.push(gamePk);
+  });
+  return games;
+};
+
+const getPlayerContent = async (player, game) => {
+  let highlights = [];
+
+  const {data} = await axios.get(
+    `http://statsapi.mlb.com/api/v1/game/${game}/content`
+  );
+  const content = data.highlights.highlights.items;
+  // console.log('player conent data is', content);
+  for (let i = 0; i < content.length; i++) {
+    const highlight = content[i];
+    if (highlight.type === 'video') {
+      console.log(highlight.description.includes(player));
+      if (highlight.description.includes(player) !== 0) {
+        highlights.push(highlight);
+      }
+    }
+  }
+  console.log('highlights are', highlights);
+  return highlights;
 };
