6d36a42a202c6946913e5871486b30efcb247ad8
Rick Terry <fdterr@gmail.com>
2019-04-08 19:41:08 -0400

    push to deploy

--- client/components/home.js
+++ client/components/home.js
@@ -1,13 +1,34 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
-import axios from 'axios';
+import {Header, Icon, Menu} from 'semantic-ui-react';
 import SearchForm from './search';
 
 const Home = props => {
   console.log('home rendered');
   return (
-    <div id="homeSearch">
-      <SearchForm history={props.history} />
+    <div>
+      <div id="homeSearch">
+        <div>
+          <Icon size="big" name="video camera" />{' '}
+          <Icon size="big" name="baseball ball" />
+        </div>
+        <Header>Search MLB Highlights...by Player</Header>
+        <SearchForm history={props.history} />
+      </div>
+      <div>
+        <Menu
+          className="footer"
+          fixed="bottom"
+          borderless
+          style={{display: 'flex', 'justify-content': 'center'}}
+        >
+          <Menu.Item
+            onClick={() => (window.location = 'https://github.com/fdterr')}
+          >
+            by Rick Terry
+          </Menu.Item>
+        </Menu>
+      </div>
     </div>
   );
 };
